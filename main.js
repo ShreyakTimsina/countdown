7a260eb362dc4c74cc5aa6516f3679cce22c8d30
Shreyak <59333955+ShreyakTimsina@users.noreply.github.com>
2020-10-20 17:59:29 +0545

    iniitial upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const countdown = document.getElementById("countdown");
+const loading = document.getElementById("loading");
+
+const birthdayTime = new Date("January 30 2021 00:00:00");
+
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = birthdayTime - currentTime;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  days.innerText = d;
+  hours.innerText = h < 10 ? "0" + h : h;
+  minutes.innerText = m < 10 ? "0" + m : m;
+  seconds.innerText = s < 10 ? "0" + s : s;
+}
+
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = "flex";
+}, 1000);
+
+setInterval(updateCountdown, 1000);
